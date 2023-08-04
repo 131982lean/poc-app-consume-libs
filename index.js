7a9d815d2d7ee2ff0397ecc048a6263b34667397
Leandro <leandrolopez131982@gmail.com>
2023-08-04 09:32:07 -0300

    terminado

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const { multiplicar, suma, } = require('poc-lib-shared');
 const { potencia } = require('poc-lib-shared-pow');
 
 console.log(multiplicar(2, 3));
-console.log(potencia(8, 3));
+console.log(potencia(5, 9));
